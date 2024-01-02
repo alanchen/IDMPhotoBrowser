ad4a50804d2e775a7565360dd8125ec6182e2b48
alanchen <mvpi3.tw@yahoo.com.tw>
2024-01-02 18:09:40 +0800

    Update IDMPhoto.m

--- Classes/IDMPhoto.m
+++ Classes/IDMPhoto.m
@@ -137,13 +137,13 @@ caption = _caption;
         } else if (_photoURL) {
             // Load async from web (using SDWebImageManager)
 			
-			[[SDWebImageManager sharedManager] loadImageWithURL:_photoURL options:SDWebImageRetryFailed progress:^(NSInteger receivedSize, NSInteger expectedSize, NSURL * _Nullable targetURL) {
+			[[SDWebImageManager sharedManager] downloadImageWithURL:_photoURL options:SDWebImageRetryFailed progress:^(NSInteger receivedSize, NSInteger expectedSize) {
 				CGFloat progress = ((CGFloat)receivedSize)/((CGFloat)expectedSize);
 				
 				if (self.progressUpdateBlock) {
 					self.progressUpdateBlock(progress);
 				}
-			} completed:^(UIImage * _Nullable image, NSData * _Nullable data, NSError * _Nullable error, SDImageCacheType cacheType, BOOL finished, NSURL * _Nullable imageURL) {
+			} completed:^(UIImage *image, NSError *error, SDImageCacheType cacheType, BOOL finished, NSURL *imageURL) {
 				if (image) {
 					self.underlyingImage = image;
 				}
